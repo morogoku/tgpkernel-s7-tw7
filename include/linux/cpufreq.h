a2664b70b7699a344f42e73804faa387295f9c65
lyapota <lyapic@ukr.net>
2016-07-15 21:33:55 +0300

    Added dancedance CPU Frequency Governor
    Signed-off-by: djb77 <dwayne.bakewell@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -504,6 +504,9 @@ extern struct cpufreq_governor cpufreq_gov_conservative;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CONSERVATIVE_X)
 extern struct cpufreq_governor cpufreq_gov_conservative_x;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_conservative_x)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DANCEDANCE)
+extern struct cpufreq_governor cpufreq_gov_dancedance;
+#define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_dancedance)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DARKNESS)
 extern struct cpufreq_governor cpufreq_gov_darkness;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_darkness)
