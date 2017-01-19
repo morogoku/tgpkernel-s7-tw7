72901e2095caacf253ac4f14586359beebdd6b24
djb77 <dwayne.bakewell@gmail.com>
2017-01-19 21:51:00 +1100

    Update Flashable Zips
    Updated for Nougat
    
    Signed-off-by: djb77 <dwayne.bakewell@gmail.com>

--- build/zip/g93xx/tgpkernel/system_patch.sh
+++ build/zip/g93xx/tgpkernel/system_patch.sh
@@ -1,7 +1,16 @@
 #!/sbin/sh
+# Created by tkkg1994, modified by djb77
 
+# Remove wrong build.prop entries
 sed -i /timaversion/d /system/build.prop
 sed -i /security.mdpp.mass/d /system/build.prop
 sed -i /ro.hardware.keystore/d /system/build.prop
-rm -rf /system/app/TuiService /system/app/mcRegistry /system/*info*
+
+# Setting correct fingerprint files
+rm -rf /system/app/TuiService /system/app/mcRegistry
+rm -f /system/vendor/lib/libsecure_storage.so
+rm -f /system/vendor/lib/libsecure_storage_jni.so
+rm -f /system/vendor/lib64/libsecure_storage.so
+rm -f /system/vendor/lib64/libsecure_storage_jni.so
+
 
