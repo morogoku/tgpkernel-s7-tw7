8fc0a0536a9d2a7a47f9a2f85d2a1ff90b22dad8
djb77 <dwayne.bakewell@gmail.com>
2017-01-22 04:53:18 +1100

    Remove broken init.d and fix Wakelock
    Removed broken init.d support, this method no longer works with N.
    Fixed Wavelock sleep timer from 40 to 1.
    Fixed Flashable zip to install wavelock.sh to either /magisk/phh/su.d or /su/su.d
    
    Signed-off-by: djb77 <dwayne.bakewell@gmail.com>

--- build/zip/g930x/tgpkernel/system_patch.sh
+++ build/zip/g930x/tgpkernel/system_patch.sh
@@ -16,3 +16,16 @@ rm -f /system/vendor/lib64/libsecure_storage_jni.so
 # Remvoe old PersonalPageService APK
 rm -rf /system/priv-app/PersonalPageService
 
+# Copy Wavelock.sh to PHH Superuser (if exists)
+if [ ! -d /magisk/phh/su.d ];then
+	cp -f /tmp/tgpkernel/wavelock.sh /magisk/phh/su.d/wavelock.sh
+	chown 0:0 /magisk/phh/su.d/wavelock.sh
+	chmod 755 /magisk/phh/su.d/wavelock.sh
+fi
+
+# Copy Wavelock.sh to SuperSU (if exists)
+if [ ! -d /su/su.d ];then
+	cp -f /tmp/tgpkernel/wavelock.sh /su/su.d/wavelock.sh
+	chown 0:0 /su/su.d/wavelock.sh
+	chmod 700 /su/su.d/wavelock.sh
+fi
